d703145d4061e5f41a11377b7df58735b13af7c9
Kenny <45161129+Gigaboi@users.noreply.github.com>
2024-08-01 13:40:47 +0300

    Add files via upload

---
+++ youtube_embed.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Get the YouTube watch link from the command line argument
+watch_link=$1
+
+# Check if the link is a YouTube watch link
+if [[ $watch_link == *"youtube.com/watch?v="* ]]; then
+    # Extract the video ID from the watch link
+    video_id=$(echo "$watch_link" | sed -n 's/.*watch?v=\([^&]*\).*/\1/p')
+
+    # Create the embed link using the video ID
+    embed_link="https://www.youtube.com/embed/$video_id"
+
+    # Open the embed link in the default web browser
+    xdg-open "$embed_link"
+elif [[ $watch_link == *"reddit.com/"* ]]; then
+    # Extract the path after /r/
+    path=$(echo $watch_link | cut -d '/' -f 4-)
+
+    # Specify the new domain
+    new_domain="https://redlib.catsarch.com"
+
+    # Construct the new URL with the specified domain
+    new_url="$new_domain/$path"
+
+    xdg-open "$new_url"
+else
+    # Open the link as it is in the default web browser
+    xdg-open "$watch_link"
+fi
